0631e7577725042965d74b65d9c66a603d91f06c
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-25 22:08:11 +0900

    provider package

--- socialize/lib/main.dart
+++ socialize/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:socialize/models/users.dart';
+import 'package:socialize/screens/services/auth.dart';
 import 'package:socialize/screens/wrapper.dart';
+import 'models/users.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,8 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- socialize/lib/screens/wrapper.dart
+++ socialize/lib/screens/wrapper.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:socialize/screens/authenticate/authenticate.dart';
 import 'package:socialize/screens/home/home.dart';
+import 'package:socialize/models/users.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //returning home or auth widget
+    final user = Provider.of<User>(context);
+    print(user);
     return Authenticate();
   }
 }
