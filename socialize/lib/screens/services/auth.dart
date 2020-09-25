1a7cc7df0c08076ce1fbf64ad95a33df3bbc4d2c
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-25 21:23:06 +0900

    custom user model, uid only

--- socialize/lib/screens/services/auth.dart
+++ socialize/lib/screens/services/auth.dart
@@ -1,15 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:socialize/models/users.dart';
 
 class AuthService {
   //private var
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //user obj based on firebaseUser
+  User _userFrombaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   //sign in anon
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFrombaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
