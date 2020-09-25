fad026bf6c575d98b3019af9c99b334716e10a98
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-25 20:50:30 +0900

    added basic app layout, wrapper, home, auth

---
+++ socialize/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:socialize/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //returning home or auth widget
+    return Home();
+  }
+}
